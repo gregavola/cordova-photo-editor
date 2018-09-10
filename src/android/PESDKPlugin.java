cc2a48d254566acea92fd7fa817392a7f7ea1625
Greg Avola <gregory.avola@gmail.com>
2018-09-10 17:55:44 -0400

    adding stickers

--- src/android/PESDKPlugin.java
+++ src/android/PESDKPlugin.java
@@ -20,17 +20,24 @@ import android.util.Log;
 import ly.img.android.PESDK;
 import ly.img.android.sdk.models.state.PESDKConfig;
 import ly.img.android.sdk.models.constant.Directory;
+import ly.img.android.sdk.models.config.StickerCategoryConfig;
+import ly.img.android.sdk.models.config.ImageStickerConfig;
+import ly.img.android.sdk.models.config.interfaces.StickerConfigInterface;
 import ly.img.android.sdk.models.state.EditorLoadSettings;
 import ly.img.android.sdk.models.state.EditorSaveSettings;
 import ly.img.android.sdk.models.state.manager.SettingsList;
 import ly.img.android.ui.activities.ImgLyIntent;
 import ly.img.android.ui.activities.PhotoEditorBuilder;
+import  ly.img.android.sdk.decoder.ImageSource;
 import ly.img.android.sdk.models.config.CropAspectConfig;
 
 public class PESDKPlugin extends CordovaPlugin {
 
     public static final int PESDK_EDITOR_RESULT = 1;
     public static boolean shouldSave = false;
+    public static JSONArray customStickers = null;
+    public static String categoryName = "Untappd Stickers";
+    public static String categoryImage = null;
     private static boolean didInitializeSDK = false;
     private CallbackContext callback = null;
 
@@ -38,9 +45,9 @@ public class PESDKPlugin extends CordovaPlugin {
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
 
-        if (!this.didInitializeSDK) {
+        if (!didInitializeSDK) {
             PESDK.init(cordova.getActivity().getApplication(), "android_license");
-            this.didInitializeSDK = true;
+            didInitializeSDK = true;
         }
     }
 
@@ -50,10 +57,17 @@ public class PESDKPlugin extends CordovaPlugin {
             // Extract image path
             JSONObject options = data.getJSONObject(0);
             String filepath = options.optString("path", "");
-            this.shouldSave = options.optBoolean("shouldSave", false);
+            customStickers = options.getJSONArray("stickers");
+            categoryName = options.optString("categoryName", "Untappd Stickers");
+            categoryImage = options.optString("categoryImage", null);
+            shouldSave = options.optBoolean("shouldSave", false);
 
-            Log.e("PHOTO_EDITOR", String.valueOf(this.shouldSave));
+            Log.e("PHOTO_EDITOR", String.valueOf(shouldSave));
             Log.e("PHOTO_EDITOR", filepath);
+            Log.e("PHOTO_EDITOR", categoryName);
+            Log.e("PHOTO_EDITOR", categoryImage);
+            Log.e("PHOTO_EDITOR", customStickers.toString());
+            Log.e("PHOTO_EDITOR", String.valueOf(customStickers.length()));
 
             Activity activity = this.cordova.getActivity();
             activity.runOnUiThread(this.present(activity, filepath, callbackContext));
@@ -71,6 +85,44 @@ public class PESDKPlugin extends CordovaPlugin {
                 if (mainActivity != null && filepath.length() > 0) {
                     SettingsList settingsList = new SettingsList();
 
+                    if (customStickers.length() != 0 && categoryImage != null) {
+                        PESDKConfig config = settingsList.getConfig();
+
+                        Log.e("PHOTOEDITOR", "true");
+
+                        ArrayList<StickerConfigInterface> customStickersList = new ArrayList<StickerConfigInterface>();
+
+                        for (int i=0; i<customStickers.length(); i++) {
+                            try {
+                                JSONObject item = customStickers.getJSONObject(i);
+
+                                String itemUrl = item.getString("image_url");
+                                String itemThumbUrl = item.getString("image_thumb_url");
+
+                                ImageStickerConfig configItem = new ImageStickerConfig(
+                                        "unique-photo-id-"+i,
+                                        "Photo Name - " +i,
+                                        ImageSource.create(Uri.parse(itemThumbUrl)),
+                                        ImageSource.create(Uri.parse(itemUrl))
+
+                                );
+
+                                customStickersList.add(configItem);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        StickerCategoryConfig stickerCategoryConfig = new StickerCategoryConfig(
+                                "Untappd Stickers",
+                                ImageSource.create(Uri.parse(categoryImage)),
+                                customStickersList
+                        );
+
+                        config.setStickerLists(stickerCategoryConfig);
+                    }
+
                     settingsList
                         .getSettingsModel(EditorLoadSettings.class)
                         .setImageSourcePath(filepath.replace("file://", ""), true) // Load with delete protection true!
@@ -86,11 +138,6 @@ public class PESDKPlugin extends CordovaPlugin {
                     new PhotoEditorBuilder(mainActivity)
                             .setSettingsList(settingsList)
                             .startActivityForResult(mainActivity, PESDK_EDITOR_RESULT);
-                } else {
-                    // Just open the camera
-                    Intent intent = new Intent(mainActivity, CameraPreviewActivity.class);
-                    callback = callbackContext;
-                    cordova.startActivityForResult(self, intent, PESDK_EDITOR_RESULT);
                 }
             }
         };
@@ -116,7 +163,7 @@ public class PESDKPlugin extends CordovaPlugin {
     private void success(Intent data) {
         String path = data.getStringExtra(ImgLyIntent.RESULT_IMAGE_PATH);
 
-        if (this.shouldSave) {
+        if (shouldSave) {
             File mMediaFolder = new File(path);
 
             MediaScannerConnection.scanFile(cordova.getActivity().getApplicationContext(),
